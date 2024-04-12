23b326c2e6cdd8b4641458f655e719fc72f0419b
Leyla N <40954611+kplr452b@users.noreply.github.com>
2024-04-12 11:51:21 +0200

    Delete animal_shelter_project/openai directory
    It is not needed

--- animal_shelter_project/openai/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-app_name = "openai"
-
-urlpatterns =[
-    path('adoption_preference/', adoption_preference_form_view, name='asoption_preference'),
-]
