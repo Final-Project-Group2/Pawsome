289d37338d2e6b4e20a94e5a57854fc72c6f7712
Mohsen <mohsen.ajalloueian@dci-student.org>
2024-04-16 09:18:46 +0200

    pushing updates for frontend

--- animal_shelter_project/animal_shelter_app/views.py
+++ animal_shelter_project/animal_shelter_app/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect, get_list_or_404
-from django.views.generic import FormView, ListView
+from django.views.generic import FormView, ListView, TemplateView
 from django.utils import timezone
 import django_filters
 import django_filters.rest_framework as filters
+from django.urls import reverse_lazy
 from rest_framework import generics
 from .models import Pet, Application
 from .serializers import PetSerializer, ApplicationSerializer
 from .forms import AddPetForm , ApplicationForm
-
+from urllib.parse import urlparse
 
 
 
@@ -43,20 +44,34 @@ class PetListCreatView(generics.ListCreateAPIView):
         return render(request, 'pet_list.html', {'pets': pets})
 
 
+class PetDetailView(generics.RetrieveUpdateDestroyAPIView):
+     queryset = Pet.objects.all()
+     serializer_class = PetSerializer
+     
+     def get(self, request, *args, **kwargs):# added by mohsen
+        instance = self.get_object()
+        pet = self.get_serializer(instance)
+        relative_path = urlparse(pet.data['photos']).path
+        shelter_name = instance.shelter.shelter_name if instance.shelter else None
 
-class PetDetailView(ListView):
-    model = Pet
-    template_name = "pet_detail.html" 
-    context_object_name = "pet"
+        return render(request, 'pet_detail.html', {'pet': pet.data , 'pet_photo_path': relative_path , 'shelter_name': shelter_name})
 
-    def get_queryset(self):
-        return Pet.objects.all()
+    
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        profile = self.get_queryset().first()
-        context['pet'] = profile
-        return context
+
+# class PetDetailView(ListView):
+#     model = Pet
+#     template_name = "pet_detail.html" 
+#     context_object_name = "pet"
+
+#     def get_queryset(self):
+#         return Pet.objects.all()
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         profile = self.get_queryset().first()
+#         context['pet'] = profile
+#         return context
 
 
 class ApplicationListCreatView(generics.ListCreateAPIView):
@@ -69,14 +84,19 @@ class ApplicationDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Application.objects.all()
     serializer_class = ApplicationSerializer
 
+
+    
+
 class AddPetView(FormView):# added by mohsen
+    model = Pet
     template_name = 'add_pet.html'
     form_class = AddPetForm
-    success_url = '/add_pet_success/'  
+    success_url = reverse_lazy('pets_list')
 
     def form_valid(self, form):
-        form.save()
         return super().form_valid(form)
+
+
 
     def get_success_url(self):
         return self.success_url
@@ -85,8 +105,8 @@ class AddPetView(FormView):# added by mohsen
 class AdoptionFormView(FormView): # added by mohsen
     template_name = 'adoption_form.html'
     form_class = ApplicationForm
-    success_url = '/adoption_success/'  
-
+    success_url = '/adopton_thank_you/' 
+    
     
     def form_valid(self, form):
         form.save()
@@ -95,4 +115,6 @@ class AdoptionFormView(FormView): # added by mohsen
     def get_success_url(self):
         return self.success_url
     
-    
+
+class ThankYouView(TemplateView):    # added by mohsen
+    template_name = 'adoption_thank_you.html'
