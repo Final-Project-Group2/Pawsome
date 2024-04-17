59233dc6b773fccb2b4f1cdf3e6c9dcd63fe0f8d
maryna-shyrokova <maryna.shyrokova@dci-student.org>
2024-04-17 13:13:05 +0200

    Updating pet details

--- animal_shelter_project/animal_shelter_app/views.py
+++ animal_shelter_project/animal_shelter_app/views.py
@@ -1,8 +1,5 @@
-from django.shortcuts import render, redirect, get_list_or_404
-from django.views.generic import FormView, ListView, TemplateView
-from django.utils import timezone
-import django_filters
-import django_filters.rest_framework as filters
+from django.shortcuts import render,get_object_or_404
+from django.views.generic import FormView, CreateView, TemplateView, UpdateView, DetailView
 from django.urls import reverse_lazy
 from rest_framework import generics
 from .models import Pet, Application
@@ -39,56 +36,64 @@ class PetListCreatView(generics.ListCreateAPIView):
             queryset = queryset.filter(species=species)
         return queryset
     
-    def get(self, request, *args, **kwargs): # added by mohsen
-        pets= self.get_queryset()
-        return render(request, 'pet_list.html', {'pets': pets})
+    def get(self, request, *args, **kwargs):
+        pets = self.get_queryset()
+        form = PetFilterForm(request.GET)
 
+        if form.is_valid():
+            species = form.cleaned_data.get('species')
+            gender = form.cleaned_data.get('gender')
+            size = form.cleaned_data.get('size')
 
-class PetDetailView(generics.RetrieveUpdateDestroyAPIView):
-     queryset = Pet.objects.all()
-     serializer_class = PetSerializer
-     
-     def get(self, request, *args, **kwargs):# added by mohsen
-        instance = self.get_object()
-        pet = self.get_serializer(instance)
-        relative_path = urlparse(pet.data['photos']).path
-        shelter_name = instance.shelter.shelter_name if instance.shelter else None
-
-        return render(request, 'pet_detail.html', {'pet': pet.data , 'pet_photo_path': relative_path , 'shelter_name': shelter_name})
-
+            if species:
+                pets = pets.filter(species=species)
+            if gender:
+                pets = pets.filter(gender=gender)
+            if size:
+                pets = pets.filter(size=size)
+        
+        return render(request, 'pet_list.html', {'pets': pets, 'form': form})
     
-
-
-# class PetDetailView(ListView):
-#     model = Pet
-#     template_name = "pet_detail.html" 
-#     context_object_name = "pet"
-
+    
+# class PetListCreatView(generics.ListCreateAPIView):
+#     queryset = Pet.objects.all()
+#     serializer_class = PetSerializer
+    
 #     def get_queryset(self):
-#         return Pet.objects.all()
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         profile = self.get_queryset().first()
-#         context['pet'] = profile
-#         return context
-
-
-class ApplicationListCreatView(generics.ListCreateAPIView):
-    queryset = Application.objects.all()
-    serializer_class = ApplicationSerializer
-    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
-    filterset_class = ApplicationFilter
-
-class ApplicationDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Application.objects.all()
-    serializer_class = ApplicationSerializer
-
-
+#         queryset = super().get_queryset()
+#         species = self.request.query_params.get('species', None)
+#         if species:
+#             queryset = queryset.filter(species=species)
+#         return queryset
     
+#     def get(self, request, *args, **kwargs): # added by mohsen
+#         pets= self.get_queryset()
+#         return render(request, 'pet_list.html', {'pets': pets})
 
-class AddPetView(FormView):# added by mohsen
+
+class PetDetailView(DetailView):
     model = Pet
+    template_name = 'pet_detail.html'
+    context_object_name = 'pet'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+        shelter = None
+
+
+        if user.is_authenticated and user.is_shelter:
+            try:
+                shelter = Shelter.objects.get(user=user)
+            except Shelter.DoesNotExist:
+                pass 
+
+        context['shelter'] = shelter
+        return context
+
+
+
+class AddPetView(LoginRequiredMixin, CreateView):
     template_name = 'add_pet.html'
     form_class = AddPetForm
     success_url = reverse_lazy('pets_list')
