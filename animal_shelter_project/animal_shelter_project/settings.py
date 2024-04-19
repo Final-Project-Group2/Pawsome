e3b4d13b0870eafbfa86448d5821d1cee35a0d5b
Mohsen <mohsen.ajalloueian@dci-student.org>
2024-04-19 09:17:14 +0200

    update shetlter_list.html

--- animal_shelter_project/animal_shelter_project/settings.py
+++ animal_shelter_project/animal_shelter_project/settings.py
@@ -93,11 +93,11 @@ WSGI_APPLICATION = 'animal_shelter_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'NAME': 'mohsen',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
